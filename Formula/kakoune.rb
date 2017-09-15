7de1fc189efc83461a1632e98dfa60503b839572
Alfie Pates <alfie@alfiepates.me>
2017-09-15 20:15:45 +0100

    kakoune: brew audit fixes

--- Formula/kakoune.rb
+++ Formula/kakoune.rb
@@ -1,19 +1,22 @@
-require "formula"
-
 class Kakoune < Formula
+  desc "mawww's experiment for a better code editor"
   homepage "https://github.com/mawww/kakoune"
   head "https://github.com/mawww/kakoune.git"
 
-  depends_on 'boost'
-  depends_on 'docbook-xsl' => :build
-  depends_on 'ncurses' => [:build, :recommended]
-  depends_on 'asciidoc' => [:build, 'with-docbook-xsl']
+  depends_on "boost"
+  depends_on "docbook-xsl" => :build
+  depends_on "ncurses" => [:build, :recommended]
+  depends_on "asciidoc" => [:build, "with-docbook-xsl"]
 
   def install
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
 
-    cd 'src' do
+    cd "src" do
       system "make", "install", "PREFIX=#{prefix}", "debug=no"
     end
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/kak --help")
   end
 end
